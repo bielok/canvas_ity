9692c1b4cf756bcd03006bf8fc4fc7cadabb7cd9
dani <138621639+bielok@users.noreply.github.com>
2025-09-11 15:10:32 -0300

    Update build_wasm.sh

--- build_wasm.sh
+++ build_wasm.sh
@@ -15,7 +15,7 @@ emcc wasm/canvas_ity_wasm.cpp \
     -std=c++14 \
     -O2 \
     -s WASM=1 \
-    -s EXPORTED_FUNCTIONS='["_init_canvas", "_destroy_canvas", "_get_image_data", "_get_width", "_get_height", "_begin_path", "_move_to", "_line_to", "_close_path", "_fill", "_stroke", "_set_fill_color", "_set_stroke_color", "_set_line_width", "_set_line_join", "_set_line_cap", "_set_shadow_blur", "_set_shadow_offset", "_set_shadow_color", "_set_linear_gradient_fill", "_add_color_stop_fill", "_set_global_composite_operation", "_fill_rect", "_clear_canvas"]' \
+    -s EXPORTED_FUNCTIONS='["_init_canvas", "_destroy_canvas", "_get_image_data", "_get_width", "_get_height", "_begin_path", "_move_to", "_line_to", "_close_path", "_fill", "_stroke", "_set_fill_color", "_set_stroke_color", "_set_line_width", "_set_line_join", "_set_line_cap", "_set_shadow_blur", "_set_shadow_offset", "_set_shadow_color", "_set_linear_gradient_fill", "_add_color_stop_fill", "_set_global_composite_operation", "_fill_rect", "_clear_canvas", "_set_line_dash", "_set_line_dash_offset"]' \
     -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "getValue", "setValue", "HEAP8", "HEAP32"]' \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s MODULARIZE=1 \
